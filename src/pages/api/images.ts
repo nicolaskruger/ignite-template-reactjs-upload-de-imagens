8d8a3d55effa57a032e870b93f003db4e6ab5b59
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-07 16:03:26 -0300

    index.tsx done

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -2,7 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fauna from 'faunadb';
 
 const { query } = fauna;
-const client = new fauna.Client({ secret: process.env.FAUNA_API_KEY });
+const client = new fauna.Client({
+  secret: process.env.FAUNA_API_KEY,
+  domain: 'db.us.fauna.com',
+  scheme: 'https',
+});
 
 interface ImagesQueryResponse {
   after?: {
@@ -79,6 +83,7 @@ export default async function handler(
         });
       })
       .catch(err => {
+        console.log(process.env.FAUNA_API_KEY);
         return res.status(400).json(err);
       });
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,42 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type FetchPagemParams = {
+  pageParam?: number;
+};
+
+type FetchDataParams = {
+  data: {
+    title: string;
+    description: string;
+    url: string;
+    ts: number;
+    id: string;
+  }[];
+  after: any;
+};
+
+type Card = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
 export default function Home(): JSX.Element {
+  const fetchPage = async ({
+    pageParam = null,
+  }: FetchPagemParams): Promise<FetchDataParams> => {
+    return (
+      await api.get(`/api/images`, {
+        params: {
+          after: pageParam,
+        },
+      })
+    ).data;
+  };
+
   const {
     data,
     isLoading,
@@ -19,17 +54,35 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    fetchPage,
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage.after) {
+          return lastPage.after;
+        }
+        return null;
+      },
+    }
     // TODO GET AND RETURN NEXT PAGE PARAM
   );
 
-  const formattedData = useMemo(() => {
+  const formattedData: Card[] = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    return (
+      data?.pages.reduce<Card[]>((acc, curr) => {
+        return [...acc, ...curr?.data];
+      }, []) || []
+    );
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
-
+  if (isLoading) {
+    return <Loading />;
+  }
   // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +90,14 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {
+          /* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */
+          hasNextPage && (
+            <Button onClick={() => fetchNextPage()}>
+              {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+            </Button>
+          )
+        }
       </Box>
     </>
   );
