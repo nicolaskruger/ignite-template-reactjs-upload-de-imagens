b3bd85d834d6af1639948d1a3e936f2b189efb83
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-07 16:23:23 -0300

    card list done

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,30 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const disclosure = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
-
+  const [imageUrl, setImageUrl] = useState('');
   // TODO FUNCTION HANDLE VIEW IMAGE
+
+  const handleViewImage = (url: string) => {
+    setImageUrl(url);
+    disclosure.onOpen();
+  };
 
   return (
     <>
       {/* TODO CARD GRID */}
-
+      <SimpleGrid column={3} spacing="40px">
+        {cards.map(card => (
+          <Card viewImage={handleViewImage} data={card} />
+        ))}
+      </SimpleGrid>
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        imgUrl={imageUrl}
+        isOpen={disclosure.isOpen}
+        onClose={disclosure.onClose}
+      />
     </>
   );
 }
