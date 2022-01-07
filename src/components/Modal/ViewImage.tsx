0f1300a348af158d532d13586b4900e73dbd83e8
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-07 16:26:26 -0300

    view image done

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,21 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay>
+        <ModalContent>
+          <ModalBody>
+            <Image src={imgUrl} />
+            <ModalFooter>
+              <Link target="_blank" href={imgUrl}>
+                abri original
+              </Link>
+            </ModalFooter>
+          </ModalBody>
+        </ModalContent>
+      </ModalOverlay>
+    </Modal>
+  );
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
 }
